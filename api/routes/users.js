6ed52a35eae671298bce02258300d0046e17bd3a
Angelo Padron <padron891@gmail.com>
2021-07-14 12:24:52 -0300

    add access token

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const router = express.Router();
 
 const User = require('../models/user');
@@ -49,6 +51,43 @@ router.post('/signup', (req, res, next) => {
             res.status(500).json({ error: err })
         });
 
+});
+
+router.post('/login', (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+            bcrypt.compare(req.body.password, user.password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    });
+                }
+                if (result) {
+                    const token = jwt.sign({ email: user.email, userId: user._id }, process.env.JWT_KEY, { expiresIn: "1h" });
+
+                    return res.status(200).json({
+                        message: 'Auth sucessful',
+                        token
+                    });
+                }
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            });
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.delete('/:userId', (req, res, next) => {

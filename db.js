641c1900d0ed403cf1cc840eac413bd375bfd0eb
Angelo Padron <padron891@gmail.com>
2021-11-23 14:24:08 -0300

    added dockunit file

---
+++ db.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.set("debug", process.env.DEBUG != undefined);
+
+const opts = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+};
+
+function open() {
+  return new Promise((resolve, reject) => {
+    if (process.env.DEBUG != undefined) {
+      let Mockgoose = require("mockgoose").Mockgoose;
+      let mockgoose = new Mockgoose(mongoose);
+      mockgoose
+        .prepareStorage()
+        .then(function () {
+          mongoose.connect(
+            `mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PW}@node-rest-shop.haj7x.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+            opts,
+            (err, res) => {
+              if (err) return reject(err);
+              resolve();
+            }
+          );
+        })
+        .catch(reject);
+    } else {
+      mongoose.connect(
+        `mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PW}@node-rest-shop.haj7x.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+        opts,
+        (err, res) => {
+          if (err) return reject(err);
+          resolve();
+        }
+      );
+    }
+  });
+}
+
+function close() {
+  return mongoose.disconnect();
+}
+
+module.exports = { close, open };
---
+++ test/test.js
@@ -0,0 +1,38 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const expect = chai.expect;
+const conn = require("../db");
+const app = require("../app");
+
+chai.use(chaiHttp);
+
+describe("Products Test Suite", function () {
+  before(function (done) {
+    conn
+      .open()
+      .then(() => done())
+      .catch(done);
+  });
+
+  after(function (done) {
+    conn
+      .close()
+      .then(() => done())
+      .catch(done);
+  });
+
+  it("No products on first call", function (done) {
+    chai
+      .request(app)
+      .get("/products")
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.count).to.equal(0);
+        expect(res.body.products.length).to.equal(0);
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
